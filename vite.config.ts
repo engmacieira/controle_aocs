48b26eeb447c0e5eb3e8235afe00b598c4f88deb
Matheus Macieira <matheus@pmb.com.br>
2026-07-20 13:18:15 -0300

    fix: atualização  vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Esta é a importação dos estilos que havia sumido!
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Aqui ativamos o Tailwind novamente
+  ],
   build: {
+    chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Separa o banco de dados do resto do Firebase
-            if (id.includes('@firebase/firestore') || id.includes('firebase/firestore')) {
-              return 'vendor-firebase-firestore';
-            }
-            if (id.includes('firebase')) {
-              return 'vendor-firebase-core';
-            }
-            if (id.includes('react')) {
-              return 'vendor-react';
-            }
+            if (id.includes('firebase')) return 'vendor-firebase';
+            if (id.includes('react')) return 'vendor-react';
             return 'vendor';
           }
-        }  
+        }
       }
     }
   }
